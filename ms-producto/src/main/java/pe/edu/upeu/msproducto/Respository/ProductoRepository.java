7b5f5b873645ded76bec408fdaeed7bde8d33095
olgeradriano <olgeradrianomeza@gmail.com>
2025-09-02 14:27:15 -0500

    cambios

--- ms-producto/src/main/java/pe/edu/upeu/msproducto/Respository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upeu.msproducto.Respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upeu.msproducto.Entity.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-}
